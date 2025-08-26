c3ab052e88e576f4326addf6e596252b67debfee
Yuankai He <yh464@cam.ac.uk>
2025-08-26 18:07:04 +0100

    Added preprocessing script for scDRS and gsMap

---
+++ ref_processing/preprocess_sc_4gsmap.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+mamba activate /rds/project/rds-Nl99R8pHODQ/toolbox/gsmap
+for dataset in gw15 gw20 gw22 gw34; do
+  gsmap run_find_latent_representations --workdir /rds/project/rds-Nl99R8pHODQ/multiomics/gsmap --sample_name qian_2025_$dataset \
+    --input_hdf5_path /rds/project/rds-Nl99R8pHODQ/multiomics/raw/qian_2025/${dataset}_full.h5ad --annotation annotation --data_layer count
+  gsmap run_latent_to_gene --workdir --workdir /rds/project/rds-Nl99R8pHODQ/multiomics/gsmap --sample_name qian_2025_$dataset \
+    --annotation annotation --latent_representation latent_GVAE --num_neighbour 51 --num_neighbour_spatial 201
+  for chrom in {1..22}; do
+    gsmap run_generate_ldscore --workdir /rds/project/rds-Nl99R8pHODQ/multiomics/gsmap --sample_name qian_2025_$dataset \
+      --chrom $chrom --bfile_root /rds/project/rds-Nl99R8pHODQ/toolbox/gsmap/gsMap_resource/LD_Reference_Panel/1000G_EUR_Phase3_plink/1000G.EUR.QC \
+      --keep_snp_root /rds/project/rds-Nl99R8pHODQ/toolbox/gsmap/gsMap_resource/LDSC_resource/hapmap3_snps/hm \
+      --gtf_annotation_file /rds/project/rds-Nl99R8pHODQ/toolbox/gsmap/gsMap_resource/genome_annotation/gtf/gencode.v46lift37.basic.annotation.gtf \
+      --enhancer_annotation_file /rds/project/rds-Nl99R8pHODQ/toolbox/gsmap/gsMap_resource/genome_annotation/enhancer/by_tissue/ALL/ABC_roadmap_merged.bed \
+      --gene_window_size 50000 --snp_multiple_enhancer_strategy max_mkscore --gene_window_enhancer_priority gene_window_first
+  done
+done
---
+++ ref_processing/preprocess_sc_4scdrs.py
@@ -0,0 +1,18 @@
+import argparse
+parser = argparse.ArgumentParser('Preprocessing single-cell data for scDRS')
+parser.add_argument('-i', dest = '_in', help = 'Input h5ad file')
+parser.add_argument('-o', dest = 'out', help = 'Output h5ad file')
+parser.add_argument('-f', dest = 'force', help = 'Force overwrite', action = 'store_true')
+args = parser.parse_args()
+
+import os
+dirname = os.path.dirname(args.out)
+if not os.path.isdir(dirname): os.system(f'mkdir -p {dirname}')
+
+import scanpy as sc
+import scdrs
+
+if not os.path.isfile(args.out) or args.force:
+  adata = scdrs.util.load_h5ad(args._in)
+  scdrs.preprocess(adata)
+  sc.write(args.out, adata)
