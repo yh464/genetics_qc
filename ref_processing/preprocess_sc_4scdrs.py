f6938642073e19cf49bff8326b658059330f3941
Yuankai He <yh464@cam.ac.uk>
2025-11-30 23:40:47 +0000

    restricted scDRS to MAGMA genes

--- ref_processing/preprocess_sc_4scdrs.py
+++ ref_processing/preprocess_sc_4scdrs.py
@@ -11,9 +11,12 @@ if not os.path.isdir(dirname): os.system(f'mkdir -p {dirname}')
 
 import scanpy as sc
 import scdrs
-
+import pandas as pd
+ref = pd.read_table('/rds/project/rds-Nl99R8pHODQ/toolbox/magma/ENSG.gene.loc', header = None).drop_duplicates(5).set_index(5)
+ref.columns = ['ENSG','CHR','START','STOP','GENE']
 if not os.path.isfile(args.out) or args.force:
   adata = scdrs.util.load_h5ad(args._in)
+  adata = adata[:,adata.var_names.isin(ref['ENSG'])]
   sc.pp.neighbors(adata, n_neighbors = 15, n_pcs = 20)
   scdrs.preprocess(adata)
   if not 'X_umap' in adata.obsm:
